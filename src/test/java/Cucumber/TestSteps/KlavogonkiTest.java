72a90bd6634be6fb3626eb5919eb41c188346cd7
Vaknasya <mr0sh9ad@gmail.com>
2023-02-24 01:33:55 +0300

    Maven framework support tied in, Cucumber, Selenide & JUnit5 dependencies added. Also created snippets for passing a given scenario in a template

---
+++ src/test/java/Cucumber/TestSteps/KlavogonkiTest.java
@@ -0,0 +1,35 @@
+package Cucumber.TestSteps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class KlavogonkiTest {
+
+    @Given("Open the desired page by pressing the button {string}")
+    public void open_the_desired_page_by_pressing_the_button(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Starting the game")
+    public void starting_the_game() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Waiting for the game to start")
+    public void waiting_for_the_game_to_start() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Enter the highlighted word in the loop")
+    public void enter_the_highlighted_word_in_the_loop() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Fix that the game is over and there are more characters per minute than {int}")
+    public void fix_that_the_game_is_over_and_there_are_more_characters_per_minute_than(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
---
+++ src/test/resources/Klavogonki.feature
@@ -0,0 +1,10 @@
+Feature: Bot for the Klavogonki website
+
+  Background: I am on the home page of the website "https://klavogonki.ru/"
+    Given Open the desired page by pressing the button "Начать игру"
+
+  Scenario: The bot starts the game and enters the words itself
+    When Starting the game
+    And Waiting for the game to start
+    And Enter the highlighted word in the loop
+    Then Fix that the game is over and there are more characters per minute than 1000
